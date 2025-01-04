90f94827993fa011399e5fb7f0ebcca85922cb94
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-04 20:53:42 +0530

    Time: 5 ms (64.41%), Space: 12.5 MB (66.63%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    int checkNum(char romanNum) {
-        if (romanNum == 'I') {
-            return 1;
-        } else if (romanNum == 'V') {
-            return 5;
-        } else if (romanNum == 'X') {
-            return 10;
-        } else if (romanNum == 'L') {
-            return 50;
-        } else if (romanNum == 'C') {
-            return 100;
-        } else if (romanNum == 'D') {
-            return 500;
-        } else if (romanNum == 'M') {
-            return 1000;
-        }
-        return 0;
-    }
-    int integer = 0;
-    int prevInt;
-    int currInt;
     int romanToInt(string s) {
-        for (int i = 0; i < s.size(); i++) {
-            currInt = checkNum(s[i]);
-            if (i != 0 && prevInt < currInt) {
-                integer = integer - prevInt + (currInt - prevInt);
-            } else {   
-                integer += currInt;
+        unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
             }
-            prevInt = currInt;
+            else{
+                ans += m[s[i]];
+            }
         }
-        return integer;
+        return ans;
     }
 };
