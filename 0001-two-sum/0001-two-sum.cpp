941c26145a103ac2467a09e1b62755b7ed5cbd19
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-06 20:07:10 +0530

    Time: 72 ms (17.93%), Space: 14.2 MB (56.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,15 +4,11 @@ public:
         vector<int> result;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-                // if (nums[i] > target) {
-                //     continue;
-                // } else {
-                    if (nums[i] + nums[j] == target) {
-                        result.push_back(i);
-                        result.push_back(j);
-                        return result;
-                    }
-                // }
+                if (nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
             }
         }
         return result;
