ca64345a7d2bc766dd98d64996fdd0079a68b699
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-08 22:07:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sizeCheckElemArr = 0;
+    int checkedElement[];
+
+    for (int i = 0; i < sizeCheckElemArr; i++) {
+        if (checkedElement[i] == nums) {
+            flag = 0
+        }
+    }
+
+
+    int majorityElement(vector<int>& nums) {
+        
+    }
+};
