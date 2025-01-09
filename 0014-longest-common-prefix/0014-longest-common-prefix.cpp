54581b4cdb736e564475531c11b11f1ca7222d96
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-09 23:02:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string commonStr = "";
-
-        if (strs.empty()) {
-            return commonStr;
-        }
-
-        sort(strs.begin(),strs.end());
-
-        int size = strs.size();
-        string firstElem = strs[0];
-        string lastElem = strs[size - 1];
-
-        for (int i = 0; i < firstElem.size(); i++) {
-            if (firstElem[i] != lastElem[i]) {
-                return commonStr;
+    string commonPref = "";
+    string checkCommon(string str1, string str2) {
+        for (int i = 0; i < str2.size(); i++) {
+            if (str1[i] == str2[i]) {
+                commonPref = str1.substr(0,i+1);
+            } else {
+                break;
             }
-            commonStr += firstElem[i];
         }
-        return commonStr;       
+        return commonPref;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 1) return strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            if (i == 1) {
+                checkCommon(strs[0], strs[1]);
+            } else {
+                checkCommon(strs[i], commonPref);
+            }
+        }
+        return commonPref;     
     }
 };
