0781ba0da5650806b0dde085f0a38afe447e25e3
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-09 23:28:23 +0530

    Time: 0 ms (100%), Space: 12.3 MB (14.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
     string commonPref = "";
-    string checkCommon(string str1, string str2) {
-        for (int i = 0; i < str2.size(); i++) {
-            if (str1[i] == str2[i]) {
-                commonPref = str1.substr(0,i+1);
-            } else {
-                commonPref = str1.substr(0,i);
-                break;
-            }
-        }
-        return commonPref;
-    }
     string longestCommonPrefix(vector<string>& strs) {
-        // if (strs.size() == 1) return strs[0];
-        // for (int i = 1; i < strs.size(); i++) {
-        //     if (i == 1) {
-        //         checkCommon(strs[0], strs[1]);
-        //     } else {
-        //         checkCommon(strs[i], commonPref);
-        //     }
-        // }
-        // return commonPref;
         sort(strs.begin(), strs.end());
         string firstStr = strs[0];
         string lastStr = strs[strs.size()-1];
@@ -35,3 +15,5 @@ public:
         return commonPref;
     }
 };
+
+// other solutions don't seem so good and understandable
