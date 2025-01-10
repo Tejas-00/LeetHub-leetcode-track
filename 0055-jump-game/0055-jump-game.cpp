22c5666372483ecefc1d05b8c6efd674d6eae263
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-10 21:51:46 +0530

    Time: 0 ms (100%), Space: 52.1 MB (66.81%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIndex = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int currIndex = nums[i] + i;
+            if (i > maxIndex) return false;
+            else maxIndex = max(maxIndex,currIndex);
+        }
+        return true;
+    }
+};
