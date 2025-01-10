0a5037297129ac8a947fa8ca44c12d11fb45099c
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-10 22:27:53 +0530

    Time: 0 ms (100%), Space: 30.5 MB (6.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -8,3 +8,12 @@ public:
         nums = result;
     }
 };
+
+// Best Solution
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         k = k % n;
+//         reverse(nums.begin(), nums.end());
+//         reverse(nums.begin(), nums.begin() + k);
+//         reverse(nums.begin() + k, nums.end());
+//     }
