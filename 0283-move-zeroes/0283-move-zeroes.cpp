784352f0ac6b7134df8cc335536ef060860ecbd5
Tejas-00 <119691963+Tejas-00@users.noreply.github.com>
2025-01-18 06:38:58 +0530

    Time: 0 ms (100%), Space: 23.7 MB (87.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0; // Pointer for tracking position of non-zero elements
+        int j = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                swap(nums[i], nums[j]); // Swap non-zero element with zero
+                swap(nums[i], nums[j]);
                 j++;
             }
         }
