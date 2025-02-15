15452da15553226f5edff049a971297e95c6a602
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-02-16 00:11:53 +0530

    Time: 0 ms (100%), Space: 13.8 MB (5.38%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) return i;
-            if (nums[i] > target) return i;
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        return nums.size();
+        
+        if (nums[mid] < target) return mid+1;
+        else return mid;
     }
 };
