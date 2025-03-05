e562a4aaaa40f101b51dc8171c9c15c4850012c1
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-03-05 23:59:54 +0530

    Time: 0 ms (100%), Space: 20.8 MB (9.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
 
-vector<int> majorityElement(vector<int> v) {
-    int n = v.size(); //size of the array
-    vector<int> ls; // list of answers
+vector<int> majorityElement(vector<int> nums) {
+    int n = nums.size();
+    vector<int> result;
 
-    //declaring a map:
     map<int, int> mpp;
 
-    // least occurrence of the majority element:
     int mini = int(n / 3) + 1;
 
-    //storing the elements with its occurnce:
     for (int i = 0; i < n; i++) {
-        mpp[v[i]]++;
+        mpp[nums[i]]++;
 
-        //checking if v[i] is
-        // the majority element:
-        if (mpp[v[i]] == mini) {
-            ls.push_back(v[i]);
+        if (mpp[nums[i]] == mini) {
+            result.push_back(nums[i]);
         }
-        if (ls.size() == 2) break;
+        if (result.size() == 2) break;
     }
 
-    return ls;
+    return result;
 }
 
 
