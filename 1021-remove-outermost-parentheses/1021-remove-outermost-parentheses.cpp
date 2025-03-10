5c96b1f69fd38123d0bd0180abd11be405ddc0c9
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-03-10 22:58:48 +0530

    Time: 0 ms (100%), Space: 8.9 MB (81.14%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int counter = 0; 
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '(') {
+                if (counter > 0) {
+                    result += s[i];
+                }
+                counter++;
+            } else {
+                counter--;
+                if (counter > 0) {
+                    result += s[i];
+                }
+            }
+        }
+        return result;
+    }
+};
