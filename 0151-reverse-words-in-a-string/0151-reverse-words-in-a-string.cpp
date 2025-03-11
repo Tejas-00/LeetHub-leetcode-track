fe1bb7a941f6759585c07bd7198bc77cc7d48e4f
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-03-11 23:45:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (71.74%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -6,6 +6,7 @@ public:
         
         for (int i = s.size()-1; i >= 0; i--) {
             if (s[i] == ' ') {
+                // removing extra spaces
                 if (i == j - 1) {
                     j--;
                     continue;
@@ -14,8 +15,10 @@ public:
                 j = i;
             }
         }
+        // adding first word
         result += s.substr(0,j);
 
+        // removing spaces at last
         while (result.back() == ' ') {
             result.pop_back();
         }
