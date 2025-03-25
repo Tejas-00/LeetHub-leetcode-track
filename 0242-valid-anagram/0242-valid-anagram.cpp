a0c29ef2487506f16a792cd3876f86dd7a5c9494
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-03-25 09:33:41 +0530

    Time: 0 ms (100%), Space: 10.1 MB (11.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,33 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t) return true;
-        return false;
+        unordered_map<char, int> count;
+        
+        // Count the frequency of characters in string s
+        for (auto x : s) {
+            count[x]++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (auto x : t) {
+            count[x]--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (auto x : count) {
+            if (x.second != 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
+
+// Mine Brute force \U0001f636‍\U0001f32b️
+// bool isAnagram(string s, string t) {
+//     sort(s.begin(), s.end());
+//     sort(t.begin(), t.end());
+//     if (s == t) return true;
+//     return false;
+// }
