dad864f861a4aae5754d431a31f7b11d33fb887f
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-03-25 09:38:27 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> count;
-        
-        // Count the frequency of characters in string s
-        for (auto x : s) {
-            count[x]++;
+        unordered_map<char, int> countMap;
+
+        for (auto i : s) {
+            countMap[i]++;
+        } for (auto i : t) {
+            countMap[i]--;
         }
-        
-        // Decrement the frequency of characters in string t
-        for (auto x : t) {
-            count[x]--;
-        }
-        
-        // Check if any character has non-zero frequency
-        for (auto x : count) {
-            if (x.second != 0) {
-                return false;
-            }
-        }
-        
-        return true;
+
+        for (auto i : countMap) {
+            if (i.second != 0) return false;
+        } return true;
     }
 };
 
@@ -31,3 +22,24 @@ public:
 //     if (s == t) return true;
 //     return false;
 // }
+
+// unordered_map<char, int> count;
+        
+//         // Count the frequency of characters in string s
+//         for (auto x : s) {
+//             count[x]++;
+//         }
+        
+//         // Decrement the frequency of characters in string t
+//         for (auto x : t) {
+//             count[x]--;
+//         }
+        
+//         // Check if any character has non-zero frequency
+//         for (auto x : count) {
+//             if (x.second != 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
