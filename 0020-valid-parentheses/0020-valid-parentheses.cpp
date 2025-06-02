fff8d1494518ff0a471909cad120e12cd504694d
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-06-03 00:22:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (96.37%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') stk.push(s[i]);
+            else {
+                if (!stk.empty()) {
+                    if (s[i] == ')' && stk.top() == '(' || s[i] == ']' && stk.top() == '[' || s[i] == '}' && stk.top() == '{') {
+                            stk.pop();
+                        }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        if (stk.empty()) return true;
+        return false;
+        return true;
+    }
+};
