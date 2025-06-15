5bd5884aa140b02123e959150a2deabcc2af4e9e
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-06-15 12:07:25 +0530

    Time: 3 ms (62.08%), Space: 23.6 MB (31.22%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+public:
+    stack<int> stk;
+    stack<int> minStk;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if (minStk.empty() || minStk.top() >= val) minStk.push(val);
+        stk.push(val);
+    }
+    
+    void pop() {
+        if (stk.top() == minStk.top()) minStk.pop();
+        stk.pop();
+    }
+    
+    int top() {
+        return(stk.top());
+    }
+    
+    int getMin() {
+        return(minStk.top());
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
