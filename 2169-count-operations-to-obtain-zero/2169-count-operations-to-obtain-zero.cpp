d785cf079cc993cd7e1de2958599a91cd5602388
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-11-07 22:50:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -3,13 +3,9 @@ public:
     int countOperations(int num1, int num2) {
         int count = 0;
         while (num1 && num2) {
-            if (num1 >= num2 ) {
-                num1 -= num2;
-                count++;
-            } else {
-                num2 -= num1;
-                count++;
-            }
+            if (num1 >= num2 ) num1 -= num2;
+            else num2 -= num1;
+            count++
         } return count;
     }
 };
