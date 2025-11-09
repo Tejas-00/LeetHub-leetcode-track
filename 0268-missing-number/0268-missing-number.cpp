d1a407de668482584765194bccc920527c4fe99d
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-11-09 09:22:28 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.27%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != i) {
-                return i;
-            }
+        int n = nums.size();
+        int expectedSum = (n * (n + 1)) / 2;
+        
+        int actualSum = 0;
+        for (int i = 0; i < n; i++) {
+            actualSum += nums[i];
         }
-        return nums.size();
+        return expectedSum - actualSum;
     }
 };
