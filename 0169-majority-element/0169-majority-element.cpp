f457eba8950baf1d78eb1db88b0c9371f6d0abda
git.tejas.11 <119691963+Tejas-00@users.noreply.github.com>
2025-12-31 00:34:16 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int index = 0;
-        int count;
-        while (count <= nums.size()/2) {
-            count = 0;
-            for (int i = 0; i < nums.size(); i++) {
-                if (nums[index] == nums[i]) count++;
-            }
-            index++;
-        }
-        return nums[index-1];
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
     }
 };
